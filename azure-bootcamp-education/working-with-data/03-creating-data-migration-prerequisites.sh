fb897cc648f8d7b0a9ff8baea608a069861f0d70
Fabrício Sanchez <fabsanc@microsoft.com>
2018-06-26 12:19:12 -0400

    Adding new script and demo explanation to Demo3

---
+++ azure-bootcamp-education/working-with-data/03-creating-data-migration-prerequisites.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Global variables
+vnetname=vnetdatamigrationmoett
+subnetname=admssubnet
+vnetprefix=10.0.0.0/16
+subnetprefix=10.0.0.0/24
+rg=rgmoett-db
+nsgname=nsgdatamigrationmoett
+location=eastus
+
+# Creating the firewall (NSG) to the subnet of our data migration process
+az network nsg create \
+    -g $rg \
+    -n $nsgname \
+    -l $location
+
+# Allowing ports required by migration process
+az network nsg rule create \
+    --resource-group $rg \
+    --nsg-name $nsgname \
+    --name open_443 \
+    --priority 100 \
+    --access Allow \
+    --direction Inbound \
+    --destination-port-ranges 443
+
+az network nsg rule create \
+    --resource-group $rg \
+    --nsg-name $nsgname \
+    --name open_53 \
+    --priority 200 \
+    --access Allow \
+    --direction Inbound \
+    --destination-port-ranges 53
+
+az network nsg rule create \
+    --resource-group $rg \
+    --nsg-name $nsgname \
+    --name open_9354 \
+    --priority 300 \
+    --access Allow \
+    --direction Inbound \
+    --destination-port-ranges 9354
+
+az network nsg rule create \
+    --resource-group $rg \
+    --nsg-name $nsgname \
+    --name open_445 \
+    --priority 400 \
+    --access Allow \
+    --direction Inbound \
+    --destination-port-ranges 445
+
+az network nsg rule create \
+    --resource-group $rg \
+    --nsg-name $nsgname \
+    --name open_12000 \
+    --priority 500 \
+    --access Allow \
+    --direction Inbound \
+    --destination-port-ranges 12000
+
+
+# Creating Azure Vnet and subnets to support data migration service
+az network vnet create \
+    -g $rg \
+    -n $vnetname \
+    --address-prefix $vnetprefix
+
+# Creating a new subnet with a NSG attached
+az network vnet subnet create \
+    -g $rg \
+    --vnet-name $vnetname \
+    -n $subnetname \
+    --address-prefix $subnetprefix \
+    --network-security-group $nsgname
+
